bc4c4b990ed602959a95baec645d6050329acb84
Christopher Roman <cr469@cornell.edu>
2018-03-08 14:23:24 -0500

    Moved lot_info here to store most recent information in memory. Added code to push all new incoming lot information to the DB

--- Server/src/app/pcasts/dao/sensor_dao.py
+++ Server/src/app/pcasts/dao/sensor_dao.py
@@ -1,5 +1,12 @@
 from . import *
-from . import pcasts
+from app.pcasts.models.parking_info import *
+
+# In-Memory Store
+lot_info = {}
 
 def add_sensor_data(lot_id, availble_spots):
-  pcasts.lot_info[lot_id] = availble_spots
+  lot_info[lot_id] = availble_spots
+  print "Added: Lot {} has {} availble spots".format(lot_id, availble_spots)
+  info = ParkingInfo(lot_id=lot_id, available_spots=availble_spots)
+  info.save()
+  print ParkingInfo.objects().count()
